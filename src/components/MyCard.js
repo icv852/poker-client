b4000ad12d1a55cdb57449b465737f737e46fb5d
icv852 <icv852@gmail.com>
2022-01-30 23:58:35 +0800

    add MyCard object

---
+++ src/components/MyCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function MyCard(props) {
+    const cardStyle = {
+        zIndex: props.card.myCardsIndex,
+        left: props.card.myCardsIndex * 40,
+        top: props.card.selected ? 0 : ""
+    }
+    return (
+        <div 
+            className="card"
+            style={cardStyle}
+            onClick={() => props.selectCard(props.card.allCardsIndex)}
+        >
+            <img src={`cards/${props.card.suit}/${props.card.number}.png`} />
+        </div>        
+    )
+}
--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,22 +1,10 @@
 import React from 'react'
+import MyCard from './MyCard'
 
 export default function MyCards(props) {
-    const cardsElements = props.cards.map(function(card) {
-        // console.log(card)
-        const cardStyle = {
-            zIndex: card.myCardsIndex,
-            left: card.myCardsIndex * 40,
-            top: card.selected ? 0 : ""
-        }
-        
+    const cardsElements = props.cards.map(function(card) {        
         return (
-            <div 
-                className="card"
-                style={cardStyle}
-                onClick={() => props.selectCard(card.allCardsIndex)}
-            >
-                <img src={`cards/${card.suit}/${card.number}.png`} />
-            </div>        
+            <MyCard selectCard={props.selectCard} card={card} />    
         )
     })
     
