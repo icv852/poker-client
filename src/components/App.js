fe497752ed5173194bb040e5054ad9b675e7fb26
icv852 <icv852@gmail.com>
2022-01-31 17:03:03 +0800

    compare cards when I play 1 card or 2 cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,17 @@
 import React from 'react'
 import CurrentBiggestContainer from './CurrentBiggest'
 import MyCards from './MyCards'
+import generateNewCards from '../logics/generateNewCards'
+import comparingCardRanks from '../logics/comparingCardRanks'
+
 
 function App() {
   const me = 0
-  const [allCards, setAllCards] = React.useState(generateNewCards())
+  const [allCards, setAllCards] = React.useState(generateNewCards(me))
   const [currentBiggest, setCurrentBiggest] = React.useState([])
-
   
-
-  function generateNewCards() {
-    //create a empty array
-    let deck = []
-    for (let i = 0; i < 52; i++) {
-      let suit;
-      let number;
-      //decide suit and number
-      if (i < 13) {
-        suit = 0;
-        number = i + 3;
-      } else if (i < 26) {
-        suit = 1;
-        number = i + 3 - 13;
-      } else if (i < 39) {
-        suit = 2;
-        number = i + 3 - 13 * 2;
-      } else {
-        suit = 3;
-        number = i + 3 - 13 * 3;
-      }
-      //push a card object to deck
-      deck.push({
-        suit,
-        number,
-        selected: false
-      })
-    }
-    
-    //shuffle cards
-    for (let i = deck.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-    
-    //distribute cards
-    for (let i = 0; i < deck.length; i++) {
-      deck[i]['allCardsIndex'] = i;
-      if (i < 13) {
-        deck[i]['owner'] = 0
-      } else if (i < 26) {
-        deck[i]['owner'] = 1
-      } else if (i < 39) {
-        deck[i]['owner'] = 2
-      } else {
-        deck[i]['owner'] = 3
-      }
-    }
-
-    //setting 'me' property to true for my hand cards
-    for (let i = 0; i < deck.length; i++) {
-      if (deck[i].owner === me) {
-        deck[i]['me'] = true;
-      } else {
-        deck[i]['me'] = false;
-      }
-    }
-    return deck
-  }
  
-  //put my cards in to a new array
+  //put my cards in to a new array called 'myCards'
   let myCards = []
   let myCardsIndex = 0;
   for (let i = 0; i < allCards.length; i++) {
@@ -99,13 +42,20 @@ function App() {
         mySelectedCardsAllIndexes.push(allCards[i].allCardsIndex)
       }
     }
-    //play the cards and render them in CurrentBiggest
-    setCurrentBiggest(mySelectedCards)
 
-    //delete the played cards in allCards state
-    setAllCards(prevAllCards => prevAllCards.filter(card => 
-      !mySelectedCardsAllIndexes.includes(card.allCardsIndex)
-    ))    
+    //comparing mySelectedCards with currentBiggest, returning boolean
+    const bigger = comparingCardRanks(mySelectedCards, currentBiggest)
+
+    //if my cards are bigger, do the following
+    if (bigger) {
+      //play the cards and render them in CurrentBiggest
+      setCurrentBiggest(mySelectedCards)
+
+      //delete the played cards in allCards state
+      setAllCards(prevAllCards => prevAllCards.filter(card => 
+        !mySelectedCardsAllIndexes.includes(card.allCardsIndex)
+      )) 
+    }       
   }
 
   //for dev
---
+++ src/logics/comparingCardRanks.js
@@ -0,0 +1,41 @@
+export default function comparingCardRanks(mine, biggest) {
+    //check if the number of my cards is valid (1,2,3 or 5)
+    const validNumbersOfCards = [1, 2, 3, 5];
+    if (!validNumbersOfCards.includes(mine.length)) return false
+
+    //check if the number of cards of mine and biggest are the same
+    if (biggest.length !== 0 && mine.length !== biggest.length) return false  
+
+    //when I play 1 card:
+    if(mine.length === 1) {
+        //if biggest not exists return true
+        if (biggest.length === 0) return true
+        //compare number
+        if(biggest[0].number > mine[0].number) return false
+        //compare suit when same number
+        else if(biggest[0].number === mine[0].number && biggest[0].suit > mine[0].suit) return false
+        else return true
+    }
+
+    //when I play 2 cards:
+    if(mine.length === 2) {
+        //if mine is not a pair return false
+        if(mine[0].number !== mine[1].number) return false    
+        //if biggest not exists return true
+        if (biggest.length === 0) return true       
+        
+        //sort my cards based on suits
+        mine.sort((a, b) => a.suit < b.suit)    
+        //compare number
+        if(biggest[0].number > mine[0].number) return false
+        //compare suit when same number
+        else if(biggest[0].number === mine[0].number) {
+            if(biggest[1].suit > mine[1].suit) return false
+        }
+        else return true      
+    }
+
+
+
+    return true
+}
