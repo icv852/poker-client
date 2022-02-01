225211c70854632d474abb815d7b54f51322ff0e
icv852 <icv852@gmail.com>
2022-02-01 18:36:38 +0800

    render handcards of player on the right

--- src/components/LeftCards.js
+++ src/components/LeftCards.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import CardBack from './CardBack'
+import { nanoid } from 'nanoid'
 
-export default function LeftCards() {
+export default function LeftCards(props) {
+    const cardsElements = function() {
+        let cardsArray = []
+        for (let i = 0; i < props.handsNum; i++) {
+            cardsArray.push(<CardBack key={nanoid()} style={{top: i * 30}}/>)
+        }
+        return cardsArray
+    }
+
+    const leftCardsStyles = {        
+        height: props.handsNum * 90,
+        top: `${120 + 170 / props.handsNum}px`
+    }
+    
     return (
-        <div>this is left cards</div>
+        <div className="left-cards-holder" style={leftCardsStyles} >
+            {cardsElements()}
+        </div>
     )
 }
