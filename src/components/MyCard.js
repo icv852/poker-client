9b01d62eb24036728475364e4fec2f9b16e28558
icv852 <icv852@gmail.com>
2022-02-04 19:18:49 +0800

    read the mouse X-coordinate when dragging my card

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -6,11 +6,19 @@ export default function MyCard(props) {
         left: props.card.myCardsIndex * 40,
         top: props.card.selected ? 0 : ""
     }
+
+    function handleDrag(e) {
+        console.log("X: " + e.clientX)
+    }
+
+
     return (
         <div 
             className="card"
             style={cardStyle}
             onClick={() => props.selectCard(props.card.myCardsIndex)}
+            draggable="true"
+            onDrag={handleDrag}
         >
             <img src={`cards/${props.card.suit}/${props.card.number}.png`} alt="card img"/>
         </div>        
