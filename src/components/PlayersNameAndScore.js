cf11fb7c104baf1e3401a572eb5a6e3d7c63f103
icv852 <icv852@gmail.com>
2022-02-04 16:07:04 +0800

    put my and opponent name and score in correct position

--- src/components/PlayersNameAndScore.js
+++ src/components/PlayersNameAndScore.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 export default function PlayersNameAndScore({players, opponents}) {    
     return (
-        <div className="playersNameAndScore">
+        <div className="playersNameAndScore">            
+            <div className="oppositeNameAndScore">{players[opponents[1]].name}: {players[opponents[1]].score}</div>
             <div className="myNameAndScore">{players[opponents[3]].name}: {players[opponents[3]].score}</div>
-            <div className="oppositeNameAndScore">{players[opponents[1]].name}: {players[opponents[3]].score}</div>
-            <div className="leftNameAndScore">{players[opponents[2]].name}: {players[opponents[3]].score}</div>
-            <div className="rightNameAndScore">{players[opponents[0]].name}: {players[opponents[3]].score}</div>
+            <div className="leftNameAndScore">{players[opponents[2]].name}: {players[opponents[2]].score}</div>
+            <div className="rightNameAndScore">{players[opponents[0]].name}: {players[opponents[0]].score}</div>
         </div>
     )
 }
+
+{/* <MyCards cards={myCards} selectCard={selectCard}/>
+        <OppositeCards handsNum={players[opponents[1]].numberOfHands} />
+        <LeftCards handsNum={players[opponents[2]].numberOfHands} />
+        <RightCards handsNum={players[opponents[0]].numberOfHands} /> */}
