1609b2e980b6186c5c5c101ffccbc9aaf8939b06
icv852 <icv852@gmail.com>
2022-02-04 16:01:28 +0800

    add score to player object in backend and render it in front end

--- src/components/PlayersNameAndScore.js
+++ src/components/PlayersNameAndScore.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function PlayersNameAndScore({players, opponents}) {    
     return (
         <div className="playersNameAndScore">
-            <div className="myNameAndScore">{players[opponents[3]].name}: 0</div>
-            <div className="oppositeNameAndScore">{players[opponents[1]].name}: 0</div>
-            <div className="leftNameAndScore">{players[opponents[2]].name}: 0</div>
-            <div className="rightNameAndScore">{players[opponents[0]].name}: 0</div>
+            <div className="myNameAndScore">{players[opponents[3]].name}: {players[opponents[3]].score}</div>
+            <div className="oppositeNameAndScore">{players[opponents[1]].name}: {players[opponents[3]].score}</div>
+            <div className="leftNameAndScore">{players[opponents[2]].name}: {players[opponents[3]].score}</div>
+            <div className="rightNameAndScore">{players[opponents[0]].name}: {players[opponents[3]].score}</div>
         </div>
     )
 }
